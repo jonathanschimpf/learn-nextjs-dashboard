df084ff3cdd26176ba1784c46be01c5cb7e3a0ce
Jawnathan <jonathan@jonathanschimpf.com>
2024-05-28 21:38:56 -0400

    tailwing.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,10 +12,17 @@ const config: Config = {
         '13': 'repeat(13, minmax(0, 1fr))',
       },
       colors: {
-        blue: {
-          400: '#2589FE',
-          500: '#0070F3',
-          600: '#2F6FEB',
+        gray: {
+          50: '#fafafa',
+          100: '#f4f4f5',
+          200: '#e4e4e7',
+          300: '#d4d4d8',
+          400: '#a1a1aa',
+          500: '#71717a',
+          600: '#52525b',
+          700: '#3f3f46',
+          800: '#27272a',
+          900: '#18181b',
         },
       },
     },
@@ -29,4 +36,5 @@ const config: Config = {
   },
   plugins: [require('@tailwindcss/forms')],
 };
+
 export default config;
